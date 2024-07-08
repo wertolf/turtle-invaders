99bbab48fcc54be5399df56ccd72e7cd924b8e21
Yuquan Chi <32032140+wertolf@users.noreply.github.com>
2024-07-08 08:36:14 +0800

    Step 3
    Shoot Lasers With the Spacebar

--- turtle_invaders.py
+++ turtle_invaders.py
@@ -1,6 +1,8 @@
 import turtle
 
 CANNON_STEP = 10
+LASER_LENGTH = 20
+LASER_SPEED = 0.2
 
 window = turtle.Screen()
 window.tracer(0)
@@ -21,6 +23,8 @@ cannon.penup()
 cannon.color(1, 1, 1)
 cannon.shape("square")
 cannon.setposition(0, FLOOR_LEVEL)
+
+lasers = []
 
 def draw_cannon():
     cannon.clear()
@@ -47,12 +51,46 @@ def move_right():
         cannon.setx(new_x)
         draw_cannon()
 
+def create_laser():
+    laser = turtle.Turtle()
+    laser.penup()
+    laser.color(1, 0, 0)
+    laser.hideturtle()
+    laser.setposition(cannon.xcor(), cannon.ycor())
+    laser.setheading(90)
+    # move laser to just above cannon tip
+    laser.forward(20)
+    # prepare to draw the laser
+    laser.pendown()  # draw lines when it moves
+    laser.pensize(5)  # lines are 5 pixels wide
+
+    lasers.append(laser)
+
+def move_laser(laser):
+    laser.clear()
+    laser.forward(LASER_SPEED)
+    laser.forward(LASER_LENGTH)
+    laser.forward(-LASER_LENGTH)
+
 window.onkeypress(move_left, "Left")
 window.onkeypress(move_right, "Right")
+window.onkeypress(create_laser, "space")
 window.onkeypress(turtle.bye, "q")
 window.listen()
 
 # draw cannon at start of game
 draw_cannon()
 
-turtle.done()
+# game loop
+while True:
+    # move all lasers
+    for laser in lasers.copy():
+        move_laser(laser)
+        # remove laser if it goes off screen
+        if laser.ycor() > TOP:
+            laser.clear()
+            laser.hideturtle()
+            lasers.remove(laser)
+            turtle.turtles().remove(laser)
+
+    window.update()
